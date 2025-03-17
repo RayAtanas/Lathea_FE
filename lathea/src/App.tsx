c02860bd780342d31e2709ac1454850c39109e67
RAY-PC\Ray <rayatanas@gmail.com>
2025-03-17 23:53:56 +0200

    Added Routes

--- lathea/src/App.tsx
+++ lathea/src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home Page/HomePage';
 import Projects from './pages/Projects/Projects'; // Adjust the path as necessary
+import Project from './pages/Project/Project';
+import AdminDashboard from './pages/Admin Dashboard/AdminDashboard';
 
 const App: React.FC = () => {
   return (
@@ -9,6 +11,8 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/project/:id" element={<Project />} />
+        <Route path="/admin" element={<AdminDashboard />} />
       </Routes>
     </Router>
   );
