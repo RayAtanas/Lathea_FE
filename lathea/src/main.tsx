ca261802ee8ba46c8e912c04d612db2a437096dc
RAY-PC\Ray <rayatanas@gmail.com>
2025-03-25 00:13:20 +0200

    Replaced favicon with lathea logo

--- lathea/src/main.tsx
+++ lathea/src/main.tsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import favicon from './assets/LatheaGroup_Logo.png';
+
+const link = document.createElement('link');
+link.rel = 'icon';
+link.href = favicon;
+document.head.appendChild(link);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
