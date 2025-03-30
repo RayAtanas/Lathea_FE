464ee983158f39183ed38535349e1f117f9a3f93
RAY-PC\Ray <rayatanas@gmail.com>
2025-03-30 23:19:39 +0300

    Added routes

--- lathea/src/App.tsx
+++ lathea/src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home Page/HomePage';
-import Projects from './pages/Projects/Projects'; // Adjust the path as necessary
+import Projects from './pages/Projects/Projects';
 import Project from './pages/Project/Project';
 import AdminDashboard from './pages/Admin Dashboard/AdminDashboard';
+import ChairmanMessage from './pages/Message From Chairman/ChairmanMessage';
+import ChooseLathea from './pages/Choose Lathea/ChooseLathea';
+import Mission from './pages/Mission/Mission';
+import Values from './pages/Values/Values';
+import MarketStudy from './pages/Market Study/MarketStudy';
+import Development from './pages/Development/Development';
+import Contracting from './pages/Contracting/Contracting';
 
 const App: React.FC = () => {
   return (
@@ -13,6 +20,13 @@ const App: React.FC = () => {
         <Route path="/projects" element={<Projects />} />
         <Route path="/project/:id" element={<Project />} />
         <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/chairman" element={<ChairmanMessage />} />
+        <Route path="/choose-lathea" element={<ChooseLathea />} />
+        <Route path="/mission" element={<Mission />} />
+        <Route path="/values" element={<Values />} />
+        <Route path="/market-studies" element={<MarketStudy />} />
+        <Route path="/development" element={<Development />} />
+        <Route path="/contracting" element={<Contracting />} />
       </Routes>
     </Router>
   );
