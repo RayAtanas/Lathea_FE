d193944f58705dd5ab56d018c563e0f298cc02a3
RAY-PC\Ray <rayatanas@gmail.com>
2025-04-20 19:43:15 +0300

    .

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-// Remove the favicon code, it's already in index.html
+// Import favicon properly
+import favicon from './assets/LatheaGroup_Logo.png'
+
+// Set favicon programmatically
+const link = document.createElement('link')
+link.rel = 'icon'
+link.type = 'image/png'
+link.href = favicon
+document.head.appendChild(link)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
